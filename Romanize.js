11ac8d98cd10840e65fa699b3f79c84e1f57bb98
George Schizas <gschizas@gmail.com>
2014-09-17 16:51:01 +0300

    Initial commit

---
+++ Romanize.js
@@ -0,0 +1,104 @@
+/**
+ * Created by gschizas on 17/9/2014.
+ */
+
+function romanizeText(greekText) {
+    var simpleTranslationGreek = "άβδέζήιίϊΐκλνξόπρσςτυύϋΰφωώ";
+    var simpleTranslationLatin = "avdeziiiiiklnxoprsstyyyyfoo";
+    var digraphTranslationGreek = "θχψ";
+    var digraphTranslationLatin = "thchps";
+    var digraphYpsilonGreek = "αεη";
+    var digraphYpsilonLatin = "aei";
+    var digraphYpsilonBeta = "βγδζλμνραάεέηήιίϊΐοόυύϋΰωώ";
+    var digraphYpsilonPhi = "θκξπστφχψ";
+
+    var result = "";
+    var cursor = 0;
+    var letter, newLetter, previousLetter, nextLetter, thirdLetter;
+    var isUpper, isUpper2;
+    while (cursor < greekText.length) {
+        letter = greekText.charAt(cursor);
+        previousLetter = (cursor > 0) ? greekText.charAt(cursor - 1) : "";
+        nextLetter = (cursor < greekText.length - 1) ? greekText.charAt(cursor + 1) : "";
+        thirdLetter = (cursor < greekText.length - 2) ? greekText.charAt(cursor + 2) : "";
+
+        isUpper = (letter.toUpperCase() == letter);
+        isUpper2 = (nextLetter.toUpperCase() == nextLetter);
+        letter = letter.toLowerCase();
+        previousLetter = previousLetter.toLowerCase();
+        nextLetter = nextLetter.toLowerCase();
+        thirdLetter = thirdLetter.toLowerCase();
+
+        if (simpleTranslationGreek.indexOf(letter) >= 0) {
+            newLetter = simpleTranslationLatin.charAt(simpleTranslationGreek.indexOf(letter));
+        }
+        else if (digraphTranslationGreek.indexOf(letter) >= 0) {
+            var diphthong_index = digraphTranslationGreek.indexOf(letter);
+            newLetter = digraphTranslationLatin.substr(diphthong_index * 2, 2);
+        }
+        else if (digraphYpsilonGreek.indexOf(letter) >= 0) {
+            newLetter = digraphYpsilonLatin.charAt(digraphYpsilonGreek.indexOf(letter));
+            if (nextLetter == "υ" || nextLetter == "ύ") {
+                if (digraphYpsilonBeta.indexOf(thirdLetter) >= 0) {
+                    newLetter += "v";
+                    cursor += 1;
+                }
+                else if (digraphYpsilonPhi.indexOf(thirdLetter) >= 0) {
+                    newLetter += "f";
+                    cursor += 1;
+                }
+            }
+        }
+        else if (letter == "γ") {
+            if (nextLetter == "γ") {
+                newLetter = "ng";
+                cursor += 1;
+            }
+            else if (nextLetter == "ξ") {
+                newLetter = "nx";
+                cursor += 1;
+            }
+            else if (nextLetter == "χ") {
+                newLetter = "nch";
+                cursor += 1;
+            }
+            else {
+                newLetter = "g";
+            }
+        }
+        else if (letter == "μ") {
+            if (nextLetter == "π") {
+                if (previousLetter.trim() == "" || thirdLetter.trim() == "") {
+                    newLetter = "b";
+                    cursor += 1;
+                }
+                else {
+                    newLetter = "mp";
+                    cursor += 1;
+                }
+            }
+            else {
+                newLetter = "m";
+            }
+        }
+        else if (letter == "ο") {
+            newLetter = "o";
+            if (nextLetter == "υ" || nextLetter == "ύ") {
+                newLetter += "u";
+                cursor += 1;
+            }
+        }
+        else {
+            newLetter = letter;
+        }
+
+        if (isUpper) {
+            newLetter = newLetter.charAt(0).toUpperCase() +
+                (isUpper2 ? newLetter.substring(1).toUpperCase() : newLetter.substring(1).toLowerCase());
+        }
+
+        result += newLetter;
+        cursor++;
+    }
+    return result;
+}
